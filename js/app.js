0f5b76ecb233a994ac60f40544b3904c8082776d
urvashi atodaria <urvashigohil@outlook.com>
2016-02-10 22:27:07 -0500

    Finished Project

---
+++ js/app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+	var i;
+	var j=prompt("Please enter a number greater than 1 for the FizzBuzz challenge");
+	for(i=1;i<=j;i++)
+	{
+		if((i%3)==0 && (i%5)!=0)
+		{
+			//alert("Number is divisible by 3");
+			$('<li class="list_items"></li>').appendTo('.list').html('<div class="content"></div><span>' + "Fizz!" + '</span>');
+		}
+		else if((i%5)==0 && (i%3)!=0)
+		{
+			$('<li class="list_items"></li>').appendTo('.list').html('<div class="content"></div><span>' + "Buzz!" + '</span>');
+		}
+		else if((i%3)==0 && (i%5)==0)
+		{
+			$('<li class="list_items"></li>').appendTo('.list').html('<div class="content"></div><span>' + "FizzBuzz!" + '</span>');
+		}
+		else{
+			$('<li class="list_items"></li>').appendTo('.list').html('<div class="content"></div><span>' + i + '</span>');
+		}
+	}
+});
